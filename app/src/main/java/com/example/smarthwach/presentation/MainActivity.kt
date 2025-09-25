672bb941a7529ecc627d9bf761025d5ee8e28b06
lilia <liliahernandez2004@gmail.com>
2025-09-25 08:36:25 -0600

    Initial commit

---
+++ app/src/main/java/com/example/smarthwach/presentation/MainActivity.kt
@@ -0,0 +1,78 @@
+/* While this template provides a good starting point for using Wear Compose, you can always
+ * take a look at https://github.com/android/wear-os-samples/tree/main/ComposeStarter to find the
+ * most up to date changes to the libraries and their usages.
+ */
+
+package com.example.smarthwach.presentation
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.Text
+import androidx.wear.compose.material.TimeText
+import androidx.wear.tooling.preview.devices.WearDevices
+import com.example.smarthwach.R
+import com.example.smarthwach.presentation.theme.SmarthWachTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val boton:Button=findViewById(R.id.boton);
+        val context=this;
+
+
+        boton.setOnClickListener {
+            Toast.makeText(context, "Alerta", Toast.LENGTH_SHORT).show();
+            val intent= Intent(this@MainActivity, OtraVentana::class.java)
+            startActivity(intent)
+        }
+    }
+}
+
+@Composable
+fun WearApp(greetingName: String) {
+    SmarthWachTheme {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colors.background),
+            contentAlignment = Alignment.Center
+        ) {
+            TimeText()
+            Greeting(greetingName = greetingName)
+        }
+    }
+}
+
+@Composable
+fun Greeting(greetingName: String) {
+    Text(
+        modifier = Modifier.fillMaxWidth(),
+        textAlign = TextAlign.Center,
+        color = MaterialTheme.colors.primary,
+        text = stringResource(R.string.hello_world, greetingName)
+    )
+}
+
+@Preview(device = WearDevices.SMALL_ROUND, showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+    WearApp("Preview Android")
+}
---
+++ app/src/main/java/com/example/smarthwach/presentation/OtraVentana.kt
@@ -0,0 +1,11 @@
+package com.example.smarthwach.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.smarthwach.R
+class OtraVentana: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.otra_ventana)
+    }
+}
